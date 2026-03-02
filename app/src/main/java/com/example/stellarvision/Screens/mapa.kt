6d0f9b683ce6d3222e8e8f582378d896d44c1e38
joel <joelfelipe0901@gmail.com>
2026-03-02 13:16:52 -0500

    Se implemento la mockup de perfil en perfil.kt, y se puso la pantalla de mapa con el mensaje API, que sera en el futuro el mapa

--- app/src/main/java/com/example/stellarvision/Screens/mapa.kt
+++ app/src/main/java/com/example/stellarvision/Screens/mapa.kt
@@ -1,14 +1,44 @@
 package com.example.stellarvision.Screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.stellarvision.R
+import com.example.stellarvision.ui.atoms.AppText
+import com.example.stellarvision.ui.molecules.AppHeader
+import org.w3c.dom.Text
 
 @Composable
 fun Mapa(controller: NavController)
 {
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AppHeader(
+            "Stellar Vision",
+            "Mapa",
+            R.drawable.vectorsvlogo,
+            "Mapa para ubicar constelaciones"
+        )
+        AppText(
+            "Mapa API",
+            MaterialTheme.typography.labelMedium,
+            MaterialTheme.colorScheme.onBackground
+        )
 
+        //Barra de navegacion
+    }
 }
 
 @Preview(showBackground = true)
