2c65445cf532b18e328f118ca743c9bc9aad372c
Phel1405 <juanfeliperodriguez11c@gmail.com>
2026-03-03 10:45:33 -0500

    Pantalla inicial con la bottom bar y una imagen estatica

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.ui.graphics)
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
