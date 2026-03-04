a23254d0da998ff1179353fccbf59611d6caa6aa
Phel1405 <juanfeliperodriguez11c@gmail.com>
2026-03-03 23:04:20 -0500

    Cambio de boton de camara y se agrego una estrella cardinal

--- app/src/main/java/com/example/stellarvision/Screens/vista360.kt
+++ app/src/main/java/com/example/stellarvision/Screens/vista360.kt
@@ -6,6 +6,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -16,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -34,6 +39,12 @@ fun Vista360(controller: NavController)
             contentDescription = "Imagen temporal 360",
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
+        )
+
+        Icon(
+            painter = painterResource(R.drawable.cardinalstar),
+            contentDescription = "Estrella cardinal",
+            modifier = Modifier.align(Alignment.TopEnd).size(60.dp).padding(end = 20.dp)
         )
 
         Box(
