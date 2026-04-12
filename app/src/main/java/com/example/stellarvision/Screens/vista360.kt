17d9da22ea82dc178024282b74bd6f4d72de699d
Phel <149622001+Phel1405@users.noreply.github.com>
2026-04-11 21:33:17 -0500

    Implementacion inicial de ubicacion

--- app/src/main/java/com/example/stellarvision/Screens/vista360.kt
+++ app/src/main/java/com/example/stellarvision/Screens/vista360.kt
@@ -1,5 +1,7 @@
 package com.example.stellarvision.Screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,11 +14,14 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -31,6 +36,7 @@ import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.stellarvision.Model.Star
@@ -39,21 +45,34 @@ import com.example.stellarvision.R
 import com.example.stellarvision.ui.atoms.CameraButton
 import com.example.stellarvision.ui.atoms.iconsNavBar
 import com.example.stellarvision.ui.templates.BottomBar
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlin.collections.emptyList
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Vista360(controller: NavController)
-{
-
+fun Vista360(controller: NavController) {
     var stars by remember { mutableStateOf<List<Star>>(emptyList()) }
     val context = LocalContext.current
+    val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+    val locationPermissionState = rememberPermissionState(locationPermission)
+
+    val locationClient = LocationServices.getFusedLocationProviderClient(context)
+    var latitude by remember { mutableDoubleStateOf(0.0) }
+    var longitude by remember { mutableDoubleStateOf(0.0) }
+    var altitude by remember { mutableDoubleStateOf(0.0) }
 
     LaunchedEffect(Unit) {
-        withContext(Dispatchers.IO){
+        withContext(Dispatchers.IO) {
             stars = getStarsFromHYG(context)
         }
+    }
+    SideEffect {
+        locationPermissionState.launchPermissionRequest()
     }
 
     /* Vista 360
@@ -83,23 +102,42 @@ fun Vista360(controller: NavController)
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
+        if (locationPermissionState.status.isGranted) {
+            if(ContextCompat.checkSelfPermission(context,locationPermission) == PackageManager.PERMISSION_GRANTED){
 
-        Column(modifier = Modifier.weight(1F), verticalArrangement = Arrangement.Center) {
-            Text("La altitud es: 0", fontSize = 21.sp)
-            Text("La longitud es: 0", fontSize = 21.sp)
+                locationClient.lastLocation.addOnSuccessListener { location ->
+                    location?.let {
+                        latitude = location.latitude
+                        longitude = location.longitude
+                        altitude = location.altitude
+                    }
+                }
+
+
+                Column(modifier = Modifier.weight(1F), verticalArrangement = Arrangement.Center) {
+                    Text("La latitud es: $latitude", fontSize = 21.sp)
+                    Text("La longitud es: $longitude", fontSize = 21.sp)
+                    Text("La altitud es: $altitude", fontSize = 21.sp)
+                }
+
+                LazyColumn(modifier = Modifier.weight(1F)) {
+                    items(stars) { star ->
+                        Text(star.toString())
+                    }
+                }
+            }
         }
-
-        LazyColumn(modifier = Modifier.weight(1F)) {
-            items(stars){ star ->
-                Text(star.toString())
+        else{
+            Button(onClick = { locationPermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
             }
         }
     }
 
-
-
 }
 
+
+// Funcion de preview, va lag por ahora
 /*@Preview(showBackground = true)
 @Composable
 fun Vista360Preview() {
