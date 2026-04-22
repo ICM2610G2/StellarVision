97ba8b448b17bb23d2ebc8ffe1f67383707e0543
joel <joelfelipe0901@gmail.com>
2026-04-21 22:22:05 -0500

    It was made some UI fixes at mensajeria.kt

--- app/src/main/java/com/example/stellarvision/screens/mensajeria.kt
+++ app/src/main/java/com/example/stellarvision/screens/mensajeria.kt
@@ -12,15 +12,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBarsPadding
+import com.example.stellarvision.common.BottomBar
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.Alignment
+import com.example.stellarvision.common.iconsNavBar
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -66,10 +68,11 @@ fun Mensajeria(controller: NavController) {
             contactos = obtenerContactos(context)
         }
     }
-
+    BottomBar(controller, iconsNavBar){paddingValues ->
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(paddingValues)
             .padding(32.dp)
     ) {
         AppText(
@@ -250,6 +253,7 @@ fun Mensajeria(controller: NavController) {
             }
         }
     }
+}
 }
 
 fun obtenerContactos(context: Context): List<ContactoSimple> {
