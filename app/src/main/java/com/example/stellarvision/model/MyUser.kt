e9ff377321280b609837c091c4d718c0334d11c3
julianasolanx <julianansolanoo@gmail.com>
2026-05-30 14:10:15 -0500

    actualice modelos

--- app/src/main/java/com/example/stellarvision/model/MyUser.kt
+++ app/src/main/java/com/example/stellarvision/model/MyUser.kt
@@ -3,6 +3,9 @@ package com.example.stellarvision.model
 const val USERS = "users/"
 
 data class MyUser(
+    val uid: String = "",
     val username: String = "",
-    val email: String = ""
+    val email: String = "",
+    val phoneNumber: String = "",
+    val profilePictureUrl: String = ""
 )
